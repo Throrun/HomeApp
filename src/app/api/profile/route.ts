b08de5b6991ad55c1a9f1cfdb958d578289ebf64
Piotr Nagórny <pinagotr@gmail.com>
2025-04-26 10:58:43 +0200

    Created routes template + exaple route

---
+++ src/app/api/profile/route.ts
@@ -0,0 +1,7 @@
+export async function GET() {
+    return Response.json({ message: 'Hello World' })//TODO Dowload data from profle table
+}
+
+// export async function POST(request: Request) { }//TODO Adding new profile
+
+// export async function DELETE(request: Request) { }//TODO Deletion of profile
