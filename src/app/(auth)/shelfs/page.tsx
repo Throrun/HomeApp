5154594b31519c6c1ffbedb7ec6e7cbc81fd027e
pikoseq9 <wekpat@wp.pl>
2025-05-27 18:18:44 +0200

    add sites /items /rooms /shelfs + connected with mainmenu

---
+++ src/app/(auth)/shelfs/page.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import Navbar from "@/app/ui/reusable/navbar";
+import Card, {CardContent} from "@/app/ui/reusable/card";
+import styles from "./page.module.css";
+
+
+export default function Items() {
+  return (
+    <main>
+      <Navbar />
+      <div className={styles.listcontainer}>
+        <div className={styles.contentWrapper}>
+          <Card name="Shelfs" columnSpan={1} rowSpan={1} content={CardContent.list}/>
+        </div>
+      </div>
+    </main>
+  );
+}
